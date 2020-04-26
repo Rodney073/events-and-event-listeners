bd4094b15c216d518051e3897135731c811840aa
Rodney073 <63658050+Rodney073@users.noreply.github.com>
2020-04-26 14:21:13 +0200

    first commit

---
+++ strings.js
@@ -0,0 +1,14 @@
+$('.func1').on('click', () => {
+    // do something
+    $ ('body').append ('<p> Yeah you clicked on me! </p>'); 
+  });
+
+  $('.func2').on('click', () => {
+    // do something
+    $('p').text('Coronavirus outbreak!'); 
+  });
+
+  $('.func3').on('click', () => {
+    // do something
+    $('button').css('background-color', 'blue'); 
+  });
